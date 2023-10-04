2a7341262726703ce23ee24027bf149e1fc85289
Alumno <ivansd122015@gmail.com>
2023-10-04 13:41:23 +0200

    edad, comida, cantante

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba{
     public static void main(String args[]) {
-        System.out.println("Sanchez Diez");
         System.out.println("20 anos en 2003");
         System.out.println("Ensaladilla rusa");
+        System.out.println("Alejandro Fernandez");
     }
 }
